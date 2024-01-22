cb3c44f3e96115c2db2f6c0b5bd2f86d2ffe1cff
lisuid2 <2150844928@qq.com>
2024-01-22 21:09:26 +0800

    upload

--- js/extend/comment/waline/waline.js
+++ js/extend/comment/waline/waline.js
@@ -1,1 +1,60 @@
-const changeContent=e=>{if(""===e)return e;return(e=[{regex:/<img.*?src="(.*?)"?[^\>]+>/gi,replacement:"[图片]"},{regex:/<a[^>]+?href=["']?([^"']+)["']?[^>]*>([^<]+)<\/a>/gi,replacement:"[链接]"},{regex:/<pre><code>.*?<\/pre>/gi,replacement:"[代码]"},{regex:/<[^>]+>/g,replacement:""}].reduce(((e,{regex:n,replacement:t})=>e.replace(n,t)),e)).length>150?e.substring(0,150)+"...":e},$asideList=document.querySelector("#card-newest-comments .aside-list"),newestCommentInit=()=>{$asideList&&getComment()},getComment=()=>{Waline.RecentComments(options={serverURL:GLOBAL_CONFIG.comment.waline.url,count:6}).then((function(e){const n=e.comments.map((e=>({content:changeContent(e.comment),avatar:e.avatar,nick:e.nick,url:`${e.url}#${e.objectId}`,date:new Date(e.insertedAt).toISOString()})));generateHtml(n)})).catch($asideList.innerHTML="无法获取评论，请确认相关配置是否正确")},generateHtml=e=>{const n=document.querySelector("#card-newest-comments .aside-list");n.innerHTML=e.length?e.map((e=>`\n    <div class='aside-list-item'>\n      <a href='${e.url}' class='thumbnail'>\n        <img src='${e.avatar}' alt='${e.nick}'>\n        <div class='name'><span>${e.nick}</span></div>\n      </a>\n      <div class='content'>\n        <a class='comment' href='${e.url}'>${e.content}</a>\n        <time class="datetime" datetime="${e.date}"></time>\n      </div>\n    </div>\n  `)).join(""):"没有评论",window.lazyLoadInstance&&window.lazyLoadInstance.update(),window.pjax&&window.pjax.refresh(n),changeTimeFormat()};
+const changeContent = (content) => {
+    if (content === '') return content;
+
+    const replacements = [
+        { regex: /<img.*?src="(.*?)"?[^\>]+>/ig, replacement: '[图片]' },
+        { regex: /<a[^>]+?href=["']?([^"']+)["']?[^>]*>([^<]+)<\/a>/gi, replacement: '[链接]' },
+        { regex: /<pre><code>.*?<\/pre>/gi, replacement: '[代码]' },
+        { regex: /<[^>]+>/g, replacement: "" }
+    ];
+
+    content = replacements.reduce((str, { regex, replacement }) => str.replace(regex, replacement), content);
+
+    return content.length > 150 ? content.substring(0, 150) + '...' : content;
+}
+
+const $asideList = document.querySelector('#card-newest-comments .aside-list')
+const newestCommentInit = () => {
+    if ($asideList) {
+        getComment()
+    }
+}
+
+const getComment = () => {
+    Waline.RecentComments(
+        options = {
+            serverURL: GLOBAL_CONFIG.comment.waline.url,
+            count: 6,
+        }
+    ).then(function (res) {
+        const walineArray = res.comments.map(item => ({
+            'content': changeContent(item.comment),
+            'avatar': item.avatar,
+            'nick': item.nick,
+            'url': `${item.url}#${item.objectId}`,
+            'date': new Date(item.insertedAt).toISOString()
+        }))
+        generateHtml(walineArray)
+    }).catch(
+        $asideList.innerHTML = "无法获取评论，请确认相关配置是否正确"
+    )
+}
+
+const generateHtml = array => {
+    const $dom = document.querySelector('#card-newest-comments .aside-list')
+    $dom.innerHTML = array.length ? array.map(item => `
+    <div class='aside-list-item'>
+      <a href='${item.url}' class='thumbnail'>
+        <img src='${item.avatar}' alt='${item.nick}'>
+        <div class='name'><span>${item.nick}</span></div>
+      </a>
+      <div class='content'>
+        <a class='comment' href='${item.url}'>${item.content}</a>
+        <time class="datetime" datetime="${item.date}"></time>
+      </div>
+    </div>
+  `).join('') : '没有评论'
+    window.lazyLoadInstance && window.lazyLoadInstance.update()
+    window.pjax && window.pjax.refresh($dom)
+    changeTimeFormat()
+}
--- js/fps.js
+++ js/fps.js
@@ -1,1 +1,51 @@
-if(null==window.localStorage.getItem("fpson")||"1"==window.localStorage.getItem("fpson")){var rAF=window.requestAnimationFrame||window.webkitRequestAnimationFrame||function(e){window.setTimeout(e,1e3/60)},frame=0,allFrameCount=0,lastTime=Date.now(),lastFameTime=Date.now(),loop=function(){var e=Date.now(),a=e-lastFameTime,o=Math.round(1e3/a);if(lastFameTime=e,allFrameCount++,frame++,e>1e3+lastTime){if((o=Math.round(1e3*frame/(e-lastTime)))<=5)var n='<span style="color:#bd0000">卡成ppt🤢</span>';else if(o<=15)n='<span style="color:red">电竞级帧率😖</span>';else if(o<=25)n='<span style="color:orange">有点难受😨</span>';else if(o<35)n='<span style="color:#9338e6">不太流畅🙄</span>';else if(o<=45)n='<span style="color:#08b7e4">还不错哦😁</span>';else n='<span style="color:#39c5bb">十分流畅🤣</span>';document.getElementById("fps").innerHTML=`FPS:${o} ${n}`,frame=0,lastTime=e}rAF(loop)};loop()}else document.getElementById("fps").style="display:none!important";
+if (window.localStorage.getItem("fpson") == undefined || window.localStorage.getItem("fpson") == "1") {
+    var rAF = function () {
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            }
+        );
+    }();
+    var frame = 0;
+    var allFrameCount = 0;
+    var lastTime = Date.now();
+    var lastFameTime = Date.now();
+    var loop = function () {
+        var now = Date.now();
+        var fs = (now - lastFameTime);
+        var fps = Math.round(1000 / fs);
+
+        lastFameTime = now;
+        // 不置 0，在动画的开头及结尾记录此值的差值算出 FPS
+        allFrameCount++;
+        frame++;
+
+        if (now > 1000 + lastTime) {
+            var fps = Math.round((frame * 1000) / (now - lastTime));
+            if (fps <= 5) {
+                var kd = `<span style="color:#bd0000">卡成ppt🤢</span>`
+            } else if (fps <= 15) {
+                var kd = `<span style="color:red">电竞级帧率😖</span>`
+            } else if (fps <= 25) {
+                var kd = `<span style="color:orange">有点难受😨</span>`
+            } else if (fps < 35) {
+                var kd = `<span style="color:#9338e6">不太流畅🙄</span>`
+            } else if (fps <= 45) {
+                var kd = `<span style="color:#08b7e4">还不错哦😁</span>`
+            } else {
+                var kd = `<span style="color:#39c5bb">十分流畅🤣</span>`
+            }
+            document.getElementById("fps").innerHTML = `FPS:${fps} ${kd}`;
+            frame = 0;
+            lastTime = now;
+        };
+
+        rAF(loop);
+    }
+
+    loop();
+} else {
+    document.getElementById("fps").style = "display:none!important"
+}
