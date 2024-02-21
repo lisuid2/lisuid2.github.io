68267613f6a07c9470d96f8eb1f92fec9a04fe80
lisuid2 <2150844928@qq.com>
2024-02-21 11:44:10 +0800

    upload

--- js/extend/comment/twikoo/twikoo.js
+++ js/extend/comment/twikoo/twikoo.js
@@ -1,1 +1,67 @@
-const changeContent=e=>{if(""===e)return e;return(e=[{regex:/<img.*?src="(.*?)"?[^\>]+>/gi,replacement:"[图片]"},{regex:/<a[^>]+?href=["']?([^"']+)["']?[^>]*>([^<]+)<\/a>/gi,replacement:"[链接]"},{regex:/<pre><code>.*?<\/pre>/gi,replacement:"[代码]"},{regex:/<[^>]+>/g,replacement:""}].reduce(((e,{regex:n,replacement:t})=>e.replace(n,t)),e)).length>150?e.substring(0,150)+"...":e},getComment=()=>{const e=document.querySelector("#card-newest-comments .aside-list");twikoo.getRecentComments({envId:GLOBAL_CONFIG.comment.twikoo.url,region:"",pageSize:6,includeReply:!0}).then((function(e){const n=e.map((e=>({content:changeContent(e.comment),avatar:e.avatar,nick:e.nick,url:`${e.url}#${e.id}`,date:new Date(e.created).toISOString()})));generateHtml(n)})).catch((function(n){e.innerHTML="无法获取评论，请确认相关配置是否正确"}))},generateHtml=e=>{const n=document.querySelector("#card-newest-comments .aside-list");n.innerHTML=e.length?e.map((e=>`\n    <div class='aside-list-item'>\n      <a href='${e.url}' class='thumbnail'>\n        <img src='${e.avatar}' alt='${e.nick}'>\n        <div class='name'><span>${e.nick}</span></div>\n      </a>\n      <div class='content'>\n        <a class='comment' href='${e.url}'>${e.content}</a>\n        <time class="datetime" datetime="${e.date}"></time>\n      </div>\n    </div>\n  `)).join(""):"没有评论",window.lazyLoadInstance&&window.lazyLoadInstance.update(),window.pjax&&window.pjax.refresh(n),changeTimeFormat()},newestCommentInit=()=>{document.querySelector("#card-newest-comments .aside-list")&&getComment()};newestCommentInit();
+const changeContent = (content) => {
+    if (content === '') return content;
+
+    const replacements = [
+        { regex: /<img.*?src="(.*?)"?[^\>]+>/ig, replacement: '[图片]' },
+        { regex: /<a[^>]+?href=["']?([^"']+)["']?[^>]*>([^<]+)<\/a>/gi, replacement: '[链接]' },
+        { regex: /<pre><code>.*?<\/pre>/gi, replacement: '[代码]' },
+        { regex: /<[^>]+>/g, replacement: "" }
+    ];
+
+    content = replacements.reduce((str, { regex, replacement }) => str.replace(regex, replacement), content);
+
+    return content.length > 150 ? content.substring(0, 150) + '...' : content;
+}
+
+const getComment = () => {
+    const $dom = document.querySelector('#card-newest-comments .aside-list')
+
+    const runTwikoo = () => {
+        twikoo.getRecentComments({
+            envId: GLOBAL_CONFIG.comment.twikoo.url,
+            region: '',
+            pageSize: 6,
+            includeReply: true
+        }).then(function (res) {
+            const twikooArray = res.map(e => ({
+                'content': changeContent(e.comment),
+                'avatar': e.avatar,
+                'nick': e.nick,
+                'url': `${e.url}#${e.id}`,
+                'date': new Date(e.created).toISOString()
+            }))
+            generateHtml(twikooArray)
+        }).catch(function (err) {
+            $dom.innerHTML = "无法获取评论，请确认相关配置是否正确"
+        })
+    }
+    runTwikoo()
+}
+
+const generateHtml = array => {
+    const $dom = document.querySelector('#card-newest-comments .aside-list')
+    $dom.innerHTML = array.length ? array.map(item => `
+    <div class='aside-list-item'>
+      <a href='${item.url}' class='thumbnail'>
+        <img src='${item.avatar}' alt='${item.nick}'>
+        <div class='name'><span>${item.nick}</span></div>
+      </a>
+      <div class='content'>
+        <a class='comment' href='${item.url}'>${item.content}</a>
+        <time class="datetime" datetime="${item.date}"></time>
+      </div>
+    </div>
+  `).join('') : '没有评论'
+    window.lazyLoadInstance && window.lazyLoadInstance.update()
+    window.pjax && window.pjax.refresh($dom)
+    changeTimeFormat()
+}
+
+const newestCommentInit = () => {
+    const $asideList = document.querySelector('#card-newest-comments .aside-list')
+    if ($asideList) {
+        getComment()
+    }
+}
+
+newestCommentInit()
--- js/extend/search/local-search.js
+++ js/extend/search/local-search.js
@@ -1,1 +1,166 @@
-window.onload=()=>{let e,t=[];const n=document.getElementById("search-mask"),c=document.querySelector("#local-search .search-dialog"),s=()=>{window.innerWidth<768&&c.style.setProperty("--search-height",window.innerHeight+"px")},o=()=>{utils.animateOut(c,"search_close .5s"),utils.animateOut(n,"to_hide 0.5s"),window.removeEventListener("resize",s)};utils.addEventListenerPjax(document.querySelector("#search-button > .search"),"click",(()=>{utils.animateIn(n,"to_show 0.5s"),c.style.display="block",setTimeout((()=>{document.querySelector("#local-search .search-box-input").focus()}),100),document.addEventListener("keydown",(function e(t){"Escape"===t.code&&(o(),document.removeEventListener("keydown",e))})),s(),window.addEventListener("resize",s)})),utils.addEventListenerPjax(document.querySelector("#local-search .search-close-button"),"click",o);let i="",a=0;const l=10;let r=[];function d(){const e=document.getElementById("search-results");document.getElementById("search-input").addEventListener("keydown",(function(n){13===n.keyCode&&(n.preventDefault(),e.innerHTML="",i=this.value.trim(),""!==i?(r=function(e){const n=new RegExp(e.split("").join(".*"),"i");return t.filter((e=>n.test(e.title)||n.test(e.content)))}(i),u(r,a),function(e){const t=Math.ceil(e/l),n=document.getElementById("search-pagination");n.innerHTML="";const c=document.createElement("ul");c.className="pagination-list";for(let e=0;e<t;e++){const t=document.createElement("li");t.className="pagination-item",t.textContent=e+1,e===a&&t.classList.add("select"),t.addEventListener("click",(function(){a=e,u(r,e),document.querySelectorAll(".pagination-item").forEach((function(e){e.classList.remove("select")})),t.classList.add("select")})),c.appendChild(t)}n.appendChild(c)}(r.length)):function(){const e=document.getElementById("search-results"),t=document.getElementById("search-pagination"),n=document.getElementById("search-tips");e.innerHTML="",t.innerHTML="",n.innerHTML=""}())}))}function u(e,t){const n=document.getElementById("search-results");n.innerHTML="";const c=document.getElementById("search-tips");c.innerHTML="";const s=t*l,o=s+l;if(!e.length){const e=document.createElement("span");return e.className="search-result-empty",e.textContent=GLOBAL_CONFIG.lang.search.empty.replace(/\$\{query}/,i),void n.appendChild(e)}e.slice(s,o).forEach((function(e){const t=document.createElement("li");t.className="search-result-item";const c=document.createElement("a");c.className="search-result-title",c.href=e.link;const s=function(e,t){const n=new RegExp(`(${t.split(" ").join("|")})`,"gi");return e.replace(n,"<em>$1</em>")}(e.title,i);c.innerHTML=s,t.appendChild(c),n.appendChild(t)}));const a=document.createElement("span");a.className="search-result-count",a.innerHTML=`共 <b>${e.length}</b> 条结果`,c.appendChild(a)}fetch("/search.xml").then((e=>e.text())).then((n=>{let c=(new DOMParser).parseFromString(n,"text/xml").getElementsByTagName("entry");for(let e=0;e<c.length;e++){let n=c[e],s=n.getElementsByTagName("title")[0].textContent,o=n.getElementsByTagName("url")[0].textContent,i=n.getElementsByTagName("content")[0].textContent;t.push({title:s,link:o,content:i})}e=lunr((function(){this.ref("link"),this.field("title",{boost:10}),this.field("content"),t.forEach((function(e){this.add(e)}),this)}))})).catch((e=>console.error("Error loading search data:",e))),d(),window.addEventListener("DOMContentLoaded",(e=>{d()}))};
+window.onload = () => {
+    let idx, store = [];
+    const $searchMask = document.getElementById("search-mask");
+    const $searchDialog = document.querySelector("#local-search .search-dialog");
+    const openSearch = () => {
+        utils.animateIn($searchMask, "to_show 0.5s");
+        $searchDialog.style.display = "block";
+        setTimeout(() => {
+            document.querySelector("#local-search .search-box-input").focus();
+        }, 100);
+        document.addEventListener("keydown", function f(event) {
+            if (event.code === "Escape") {
+                closeSearch();
+                document.removeEventListener("keydown", f);
+            }
+        });
+        fixSafariHeight();
+        window.addEventListener("resize", fixSafariHeight);
+    };
+    const fixSafariHeight = () => {
+        if (window.innerWidth < 768) {
+            $searchDialog.style.setProperty("--search-height", window.innerHeight + "px");
+        }
+    };
+    const closeSearch = () => {
+        utils.animateOut($searchDialog, "search_close .5s");
+        utils.animateOut($searchMask, "to_hide 0.5s");
+        window.removeEventListener("resize", fixSafariHeight);
+    };
+    utils.addEventListenerPjax(document.querySelector("#search-button > .search"), "click", openSearch);
+    utils.addEventListenerPjax(document.querySelector("#local-search .search-close-button"), "click", closeSearch);
+    function initLunr() {
+        fetch("/search.xml")
+            .then(response => response.text())
+            .then(data => {
+                let parser = new DOMParser();
+                let xmlDoc = parser.parseFromString(data, "text/xml");
+                let entries = xmlDoc.getElementsByTagName("entry");
+                for (let i = 0; i < entries.length; i++) {
+                    let entry = entries[i];
+                    let title = entry.getElementsByTagName("title")[0].textContent;
+                    let link = entry.getElementsByTagName("url")[0].textContent;
+                    let content = entry.getElementsByTagName("content")[0].textContent;
+                    store.push({
+                        'title': title,
+                        'link': link,
+                        'content': content
+                    });
+                }
+
+                idx = lunr(function () {
+                    this.ref('link');
+                    this.field('title', {boost: 10});
+                    this.field('content');
+
+                    store.forEach(function (doc) {
+                        this.add(doc);
+                    }, this);
+                });
+            })
+            .catch(err => console.error("Error loading search data:", err));
+    }
+    let query = ''
+    let currentPage = 0;
+    const resultsPerPage = 10;
+    let results = [];
+
+    function initUI() {
+        const $results = document.getElementById("search-results");
+        const $search = document.getElementById("search-input");
+        $search.addEventListener('keydown', function (e) {
+            if (e.keyCode === 13) {
+                e.preventDefault();
+                $results.innerHTML = '';
+                query = this.value.trim();
+                if (query !== '') {
+                    results = search(query);
+                    renderResults(results, currentPage);
+                    renderPagination(results.length);
+                } else {
+                    clearSearchResults();
+                }
+            }
+        });
+    }
+    function clearSearchResults() {
+        const $results = document.getElementById("search-results");
+        const $pagination = document.getElementById("search-pagination");
+        const $tips = document.getElementById("search-tips");
+    
+        $results.innerHTML = '';
+        $pagination.innerHTML = '';
+        $tips.innerHTML = '';
+    }
+    function search(query) {
+        const regex = new RegExp(query.split('').join('.*'), 'i');
+        return store.filter(page => regex.test(page.title) || regex.test(page.content));
+    }    
+    function renderResults(results, page) {
+        const $search_results = document.getElementById("search-results");
+        $search_results.innerHTML = '';
+        const $tips = document.getElementById("search-tips");
+        $tips.innerHTML = '';
+        const start = page * resultsPerPage;
+        const end = start + resultsPerPage;
+        if (!results.length) {
+            const $empty = document.createElement("span");
+            $empty.className = "search-result-empty";
+            $empty.textContent = GLOBAL_CONFIG.lang.search.empty.replace(/\$\{query}/, query);
+            $search_results.appendChild($empty);
+            return;
+        }
+        results.slice(start, end).forEach(function (result) {
+            const $result = document.createElement("li");
+            $result.className = "search-result-item";
+            const $link = document.createElement("a");
+            $link.className = "search-result-title";
+            $link.href = result.link;
+            const title = highlightSearchKeyword(result.title, query);
+            $link.innerHTML = title;
+            $result.appendChild($link);
+            $search_results.appendChild($result);
+        });
+        const count = document.createElement("span");
+        count.className = "search-result-count";
+        count.innerHTML = `共 <b>${results.length}</b> 条结果`;
+        $tips.appendChild(count);
+    }
+    
+    function highlightSearchKeyword(text, keyword) {
+        const regex = new RegExp(`(${keyword.split(' ').join('|')})`, 'gi');
+        return text.replace(regex, '<em>$1</em>');
+    }
+    
+    function renderPagination(totalResults) {
+        const totalPages = Math.ceil(totalResults / resultsPerPage);
+        const paginationContainer = document.getElementById("search-pagination");
+        paginationContainer.innerHTML = '';
+        const paginationList = document.createElement("ul");
+        paginationList.className = "pagination-list";
+
+        for (let i = 0; i < totalPages; i++) {
+            const button = document.createElement("li");
+            button.className = "pagination-item";
+            button.textContent = i + 1;
+            if (i === currentPage) {
+                button.classList.add('select');
+            }
+            button.addEventListener('click', function () {
+                currentPage = i;
+                renderResults(results, i);
+                document.querySelectorAll(".pagination-item").forEach(function (btn) {
+                    btn.classList.remove('select');
+                });
+                button.classList.add('select');
+            });
+            paginationList.appendChild(button);
+        }
+        paginationContainer.appendChild(paginationList);
+    }
+    initLunr();
+    initUI();
+    window.addEventListener('DOMContentLoaded', (event) => {
+        initUI();
+    });
+}
